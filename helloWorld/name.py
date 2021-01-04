74cb090ffab38c1418ee1c76cc570609a60da1d7
Gary <roulstone.gary@gmail.com>
2021-01-04 13:43:55 +0000

    add casing validation

--- helloWorld/name.py
+++ helloWorld/name.py
@@ -1,6 +1,6 @@
 userName = input("Who are you?")
     
-while userName != "Gary":
+while userName.lower() != "gary":
     print("I don't know a", userName)
     userName = input("Who are you?")
 print("Welcome back", userName)
